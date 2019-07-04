157361e450ba0cfd0ada9220ff83740320c78874
Olivaaaa <2453273909@qq.com>
2019-07-04 22:02:09 +0800

    jpa实体注解映射为表格

--- demo1/src/main/java/demo/demo1/model/User.java
+++ demo1/src/main/java/demo/demo1/model/User.java
@@ -3,16 +3,43 @@ package demo.demo1.model;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name;
+    @Column(name = "account_id")
     private String accountId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "token")
     private String token;
+
+    @Column(name = "gmt_create")
     private Long gmtCreate;
+
+    @Column(name = "gmt_modified")
     private Long gmtModified;
+
+    public User(){
+
+    }
+
+    public User(int id){
+        this.id = id;
+    }
+
+    public User(int id, String accountId, String name, String token, Long gmtCreate, Long gmtModified){
+        this.id = id;
+        this.accountId = accountId;
+        this.name = name;
+        this.token = token;
+        this.gmtCreate = gmtCreate;
+        this.gmtModified = gmtModified;
+    }
 
     public Integer getId() {
         return id;
